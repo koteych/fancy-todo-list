3d8191ef23e7bb9c2404cbfc1317d3f1331d5eac
koteych <154607050+koteych@users.noreply.github.com>
2023-12-22 22:25:00 +0300

    Add stub to make sure requests to api work fine

--- app/app.js
+++ app/app.js
@@ -1,1 +1,14 @@
-console.log('init');
+
+fetch('http://localhost:4400/api/todos')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Fetch error:', error);
+  });
