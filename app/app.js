5f60240495329516877e8722fd0a60cb9bb5689f
koteych <154607050+koteych@users.noreply.github.com>
2023-12-24 02:32:46 +0300

    Make undone filtering work

--- app/app.js
+++ app/app.js
@@ -7,11 +7,15 @@ const DEFAULT_DATE = '2022-10-07';
  *  This is required for proper sorting and filtering
 */
 let todos = [];
+let onlyUndoneFlag = false;
 
 const todoListContainer = document.querySelector('.todo-list');
 const searchButton = document.querySelector('.search-button');
 const searchField = document.querySelector('.search-field');
 const selectedDate = document.querySelector('.selected-date');
+const todayButton = document.querySelector('.today-button');
+const thisWeekButton = document.querySelector('.this-week-button'); 
+const onlyUndoneCheckbox = document.querySelector('.only-undone-todos');
 
 todoListContainer.addEventListener('click', (e) => {
   if (String(e.target.tagName).toLowerCase() === 'button') {
@@ -24,6 +28,38 @@ todoListContainer.addEventListener('click', (e) => {
   }
 })
 
+
+/** Display only undone todos */
+onlyUndoneCheckbox.addEventListener('click', (e) => {
+  onlyUndoneFlag = !!onlyUndoneCheckbox.checked;
+  const onlyDoneTodos = todos.filter((todo) => {
+    if (onlyUndoneFlag) {
+      return todo.status === false;
+    }
+    return true;
+  });
+  updateTodoList(todoListContainer, onlyDoneTodos);
+})
+
+/**
+ * This week button
+ */
+thisWeekButton.addEventListener('click', (e) => {
+    console.log('getting todos for this week')
+})
+
+/**
+ * Today button
+ */
+todayButton.addEventListener('click', (e) => {
+  getTodosForToday((data) => {
+    updateTodosWithData(data);
+    const onlyDoneTodos = todos.filter((todo) => (true));
+    updateTodoList(todoListContainer, onlyDoneTodos);
+  })
+})
+
+
 /**
  * Search field handling
  */
@@ -31,7 +67,7 @@ searchButton.addEventListener('click', (e) => {
   const searchString = searchField.value;
   getTodoByName(searchString, (data) => {
     updateTodosWithData(data);
-    const onlyDoneTodos = todos.filter((todo) => (false));
+    const onlyDoneTodos = todos.filter((todo) => (true));
     updateTodoList(todoListContainer, onlyDoneTodos);
   })
 })
@@ -83,6 +119,6 @@ function updateTodosWithData(data) {
   todos = [];
 
   for (const retrievedTodo of data) {
-    todos.push(new Todo(retrievedTodo.id, retrievedTodo.name, retrievedTodo.shortDesc, retrievedTodo.fullDesc, retrievedTodo.date));
+    todos.push(new Todo(retrievedTodo.id, retrievedTodo.name, retrievedTodo.shortDesc, retrievedTodo.fullDesc, retrievedTodo.date, retrievedTodo.status));
   }
 }
--- app/fetcher.js
+++ app/fetcher.js
@@ -3,6 +3,7 @@ function getSome() {
 }
 
 function makeRequest(url, callback) {
+    console.log('Performing a query: ' + url);
     fetch(url)
     .then(response => {
       if (!response.ok) {
@@ -18,6 +19,16 @@ function makeRequest(url, callback) {
     });
 }
 
+function getTodosByDateRange(startDate, endDate) {
+    const formattedStartDate = (new Date(startDate)).getTime();
+    const formattedEndDate = (new Date(endDate)).getTime();
+
+    const url = `http://localhost:4400/api/todos/date?from=${formattedStartDate}&to=${formattedEndDate}`;
+    console.log(url);
+
+    makeRequest(url, callback);
+}
+
 function getTodoByDate(date, callback) {
     const formattedDate = (new Date(date)).getTime();
     const millisPerDay = 86400000;
@@ -30,6 +41,21 @@ function getTodoByDate(date, callback) {
 
 function getTodoByName(name, callback) {
     const url = `http://localhost:4400/api/todos/find?q=${name}`;
+    console.log(url);
+
+    makeRequest(url, callback);
+}
+
+function getTodosForToday(callback) {
+    getTodoByDate(new Date(), callback);
+}
+
+/** This is not what we're looking for by the way */
+function getTodosForNextWeek(callback) {
+    const formattedDate = (new Date(date)).getTime();
+    const millisPerWeek = 86400000*7;
+
+    const url = `http://localhost:4400/api/todos/date?from=${formattedDate}&to=${formattedDate + millisPerWeek}`;
     console.log(url);
 
     makeRequest(url, callback);
